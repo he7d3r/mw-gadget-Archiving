ff97f31dd91d091866eb25f6f46ddb11eeda60b1
Helder <he7d3r@gmail.com>
2012-07-04 15:19:49 +0000

    Use .done() and .fail() from promises instead of "success" and "error" callbacks
    See
    * http://api.jquery.com/deferred.done/
    * http://api.jquery.com/deferred.fail/
    
    Also, do some fixes suggested by JSLint

--- src/Arquivamento.js
+++ src/Arquivamento.js
@@ -1,8 +1,12 @@
-/*
+/**
  * Insere um link no topo da página para listar os tópicos da esplanada que já podem ser arquivados
  * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Arquivamento.js]] ([[File:User:Helder.wiki/Tools/Arquivamento.js]])
  */
+/*jslint browser: true, white: true, devel: true, plusplus: true*/
+/*global jQuery, mediaWiki, jsMsg */
+( function ( $, mw ) {
+'use strict';
 
 function checkDateOfLastEdit( data ) {
 	var	page, pages, pageids, i, topicDate, daysSinceLastComment,
@@ -11,23 +15,22 @@ function checkDateOfLastEdit( data ) {
 		today = new Date(),
 		toArchive = [],
 		text = '';
-	if ( 'error' in data ) {
+	if ( typeof data.error !== 'undefined' ) {
 		text = 'Erro ao usar a API: ' + data.error.code + '. ' + data.error.info;
 	} else if ( data.query && data.query.pages && data.query.pageids ) {
 		pages = data.query.pages;
 		pageids = data.query.pageids;
 		for (i = 0; i < pageids.length; i++) {
 			page = pages[ pageids[i] ];
-			if ( !page.pageid ) {
-				continue;
-			}
-			topicDate = new Date( page.revisions[0].timestamp );
-			daysSinceLastComment = Math.floor( ( today.getTime() - topicDate.getTime() )/milisecondsInOneDay );
-			if ( daysSinceLastComment > max ) {
-				toArchive.push(
-					'<tr><td><a href="' + mw.util.wikiGetlink( page.title ) + '?diff=0">' +
-					page.title.substr( mw.config.get('wgPageName').length + 1 ) + '</a></td><td>' + daysSinceLastComment + '</td></tr>'
-				);
+			if ( page.pageid ) {
+				topicDate = new Date( page.revisions[0].timestamp );
+				daysSinceLastComment = Math.floor( ( today.getTime() - topicDate.getTime() )/milisecondsInOneDay );
+				if ( daysSinceLastComment > max ) {
+					toArchive.push(
+						'<tr><td><a href="' + mw.util.wikiGetlink( page.title ) + '?diff=0">' +
+						page.title.substr( mw.config.get('wgPageName').length + 1 ) + '</a></td><td>' + daysSinceLastComment + '</td></tr>'
+					);
+				}
 			}
 		}
 		if ( toArchive.length > 0 ) {
@@ -71,11 +74,11 @@ function getListOfTopicsAndCheckDateOfLastEdit(){
 			'prop': 'revisions',
 			'rvprop': 'timestamp',
 			'indexpageids': '1'
-		},
-		success: checkDateOfLastEdit,
-		error: function() {
-			jsMsg( 'Houve um erro ao tentar usar a API para acessar a página atual.' );
 		}
+	})
+	.done( checkDateOfLastEdit )
+	.fail( function() {
+		jsMsg( 'Houve um erro ao tentar usar a API para acessar a página atual.' );
 	});
 }
 
@@ -87,7 +90,9 @@ if ($.inArray(mw.config.get('wgAction'), ['view', 'submit', 'purge']) !== -1) {
 		'ca-archive-list',
 		'Listar tópicos que possivelmente já podem ser arquivados'
 	) ).click( function (e) {
-		e.preventDefault(); // prevent '#' from appearing in URL bar
+		e.preventDefault();
 		mw.loader.using( 'jquery.tablesorter', getListOfTopicsAndCheckDateOfLastEdit );
 	} );
 }
+
+}( jQuery, mediaWiki ) );
